79d10a45ce3480a36de47b317a3ffa84bb3c7c47
Mmazzerratti <79002835+nekocode2006@users.noreply.github.com>
2023-07-29 23:39:56 +0300

    Update Module [AnimatedQuotes].py

--- Module [AnimatedQuotes].py
+++ Module [AnimatedQuotes].py
@@ -1,47 +1,49 @@
-# meta developer: @Mmazzerratti
+# meta developer: @Mmazzerratti @MajorOfPain
 
 from telethon.tl.types import Message
 from .. import loader, utils
 from random import randint
+import re
 import asyncio
 
 
 @loader.tds
 class AnimatedQuotes(loader.Module):
-    strings = {
+    strings_ru = {
+        "name": "Модуль [AnimatedQuotes]",
+        "no_text": (
+            "🚫 <b>Укажи текст для создания стикера</b>"
+        ),
+        "processing": (
+            "⌛️ <b>Думаю...</b>"
+        ),
+        "erroeaq": (
+            "<b>Ошибка</b>"
+        ),
+        "_cmd_doc_aniq": "<index> <text> - Создать рандомный анимированный стикер",
+        "_cls_doc": "Модуль, который создает анимированные стикеры",
+    }
+
+    strings_en = {
         "name": "Module [AnimatedQuotes]",
         "no_text": (
-            "<emoji document_id=5312526098750252863>🚫</emoji> <b>Укажи текст для"
-            " создания стикера</b>"
+            "🚫 <b>Specify the text for creating a sticker</b>"
         ),
         "processing": (
-            "<emoji document_id=5451646226975955576>⌛️</emoji> <b>Думаю...</b>"
+            "⌛️ <b>Think...</b>"
         ),
+        "erroeaq": (
+            "<b>Error</b>"
+        ),
+        "_cmd_doc_aniq": "<index> <text> - Create a random animated sticker",
+        "_cls_doc": "A module that creates animated stickers",
     }
 
-    strings_ru = {
-        "no_text": (
-            "<emoji document_id=5312526098750252863>🚫</emoji> <b>Укажи текст для"
-            " создания стикера</b>"
-        ),
-        "processing": (
-            "<emoji document_id=5451646226975955576>⌛️</emoji> <b>Думаю...</b>"
-        ),
-        "_cmd_doc_aniq": "<text> - Создать рандомный анимированный стикер",
-        "_cmd_doc_aniq1": "<text> - Создать анимированный стикер с рамкой сообщения",
-        "_cmd_doc_aniq2": "<text> - Создать анимированный стикер девочки с листочком",
-        "_cmd_doc_aniq3": "<text> - Создать анимированный стикер девочки с табличкой",
-        "_cmd_doc_aniq4": "<text> - Создать анимированный стикер акулы с рамкой сообщения",
-        "_cls_doc": "Простенький модуль, который создает анимированные стикеры",
-    }
-
-
-    async def generatefoto(self, message: Message, image_index: int):
-        args = utils.get_args_raw(message)
+    async def generatefoto(self, message: Message, image_index: int, args: str):
         if not args:
             await utils.answer(message, self.strings["no_text"])
             return
-        message = await utils.answer(message, self.strings["processing"])
+        processing_message = await utils.answer(message, self.strings["processing"])
         max_retries = 5
         retry_delay = 5
         success = False
@@ -49,34 +51,39 @@ class AnimatedQuotes(loader.Module):
             try:
                 query = await self._client.inline_query("@QuotAfBot", args)
                 if query and len(query) >= image_index + 1:
-                    await message.respond(file=query[image_index].document)
+                    await processing_message.respond(file=query[image_index].document)
                     success = True
                     break
             except Exception:
                 await asyncio.sleep(retry_delay)
                 continue
         if not success:
-            await utils.answer(message, "Failed to fetch animated quote. Please try again later.")
+            await utils.answer(message, self.strings["erroeaq"])
         if message.out:
             await message.delete()
 
 
     async def aniqcmd(self, message: Message):
-        random_index = randint(0, 3)
-        await self.generatefoto(message, random_index)
-
-
-    async def aniq1cmd(self, message: Message):
-        await self.generatefoto(message, 0)
-
-
-    async def aniq2cmd(self, message: Message):
-        await self.generatefoto(message, 1)
-
-
-    async def aniq3cmd(self, message: Message):
-        await self.generatefoto(message, 2)
-
-
-    async def aniq4cmd(self, message: Message):
-        await self.generatefoto(message, 3)
+        args = utils.get_args_raw(message).strip()
+        if args.startswith('1 '):
+            image_index = 0
+            text = args[2:].strip()
+        elif args.startswith('2 '):
+            image_index = 1
+            text = args[2:].strip()
+        elif args.startswith('3 '):
+            image_index = 2
+            text = args[2:].strip()
+        elif args.startswith('4 '):
+            image_index = 3
+            text = args[2:].strip()
+        elif args.startswith('5 '):
+            image_index = 4
+            text = args[2:].strip()
+        elif args.isdigit():
+            image_index = int(args)
+            text = ""
+        else:
+            image_index = randint(0, 4)
+            text = args
+        await self.generatefoto(message, image_index, text)
